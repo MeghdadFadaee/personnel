57102f7c01d09e1c26f3d416202592735abacfb9
Meghdad <mmf9484943@gmail.com>
2024-09-04 18:29:37 +0330

    complete update repository Widget

--- resources/views/filament/resources/widgets/update-repository.blade.php
+++ resources/views/filament/resources/widgets/update-repository.blade.php
@@ -1,13 +1,6 @@
 <x-filament-widgets::widget class="fi-account-widget">
     <x-filament::section>
         <div class="flex items-center gap-x-3">
-            <div class="flex-1">
-                <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white" dir="ltr">
-                    {{ $reset }}
-                    <br>
-                    {{ $pull }}
-                </h2>
-            </div>
 
             <x-filament::button
                     color="info"
@@ -18,6 +11,15 @@
             >
                 {{ __('Update') }}
             </x-filament::button>
+
+            <div class="flex-1">
+                <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white" dir="ltr">
+                    {{ $reset }}
+                    <br>
+                    {{ $pull }}
+                </h2>
+            </div>
+
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
