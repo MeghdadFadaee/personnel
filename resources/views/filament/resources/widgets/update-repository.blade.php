6bb3b310f4eccc85fddcb421290a5c83d875bea1
Meghdad <mmf9484943@gmail.com>
2024-09-04 18:27:28 +0330

    complete update repository Widget

--- resources/views/filament/resources/widgets/update-repository.blade.php
+++ resources/views/filament/resources/widgets/update-repository.blade.php
@@ -3,7 +3,9 @@
         <div class="flex items-center gap-x-3">
             <div class="flex-1">
                 <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
-                    {{ $ttt }}
+                    {{ $reset }}
+                    <br>
+                    {{ $pull }}
                 </h2>
             </div>
 
