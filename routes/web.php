af9658d2ba0faba0a38e677eedd56135b88e6709
Meghdad <mmf9484943@gmail.com>
2024-10-24 13:22:15 +0330

    testing

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,28 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-Route::view('eitaa', 'eitaa');
+Route::view('/eitaa', 'eitaa');
 
 Route::get('/re', function () {
+    echo '<script src="https://developer.eitaa.com/eitaa-web-app.js"></script>';
 
-    dd([
-        'headers' => request()->header(),
-        'body' => request()->all(),
-        'full_url' => request()->fullUrl(),
-        'ip' => request()->ip(),
-        'method' => request()->method(),
-        'url' => request()->url(),
-        'user_agent' => request()->userAgent(),
-    ]);
+    dd(
+        [
+            'eitaa-web-app' => true,
+        ],
+        [
+            'headers' => array_map(fn($value) => Arr::join($value, ' '), request()->header()),
+            'body' => request()->all(),
+            'full_url' => request()->fullUrl(),
+            'ip' => request()->ip(),
+            'method' => request()->method(),
+            'url' => request()->url(),
+            'user_agent' => request()->userAgent(),
+        ]
+    );
 
     return view('eitaa');
 });
