a73356f2941e92ee8441aa254d6aed6a31d8e51f
Meghdad <mmf9484943@gmail.com>
2024-10-24 12:58:10 +0330

    add route for testing eitaa

---
+++ resources/views/eitaa.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}"/>
+
+        <title>{{ config('app.name') }}</title>
+
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+
+
+    <script src="https://developer.eitaa.com/eitaa-web-app.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+Route::view('eitaa', 'eitaa');
+
+Route::get('/re', function () {
+
+    dd([
+        'headers' => request()->header(),
+        'body' => request()->all(),
+        'ip' => request()->ip(),
+        'method' => request()->method(),
+        'url' => request()->url(),
+        'user_agent' => request()->userAgent(),
+    ]);
+
+    return view('eitaa');
 });
