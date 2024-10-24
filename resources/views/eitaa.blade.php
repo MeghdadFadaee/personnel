16133a5324cd999d8abd20807945c7f0cb4b9e48
Meghdad <mmf9484943@gmail.com>
2024-10-24 15:18:22 +0330

    testing

--- resources/views/eitaa.blade.php
+++ resources/views/eitaa.blade.php
@@ -14,83 +14,9 @@
 
     <h2>Eitaa Web App</h2>
 
-    <p id="p_token"></p>
 
 
     <script>
-
-        let data = 'user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3788475317572404878&chat_type=private&auth_date=1709144340&hash=371697738012ebd26a111ace4aff23ee265596cd64026c8c3677956a85ca1827';
-        data = window.Eitaa.WebApp.initData;
-
-        const token = '60307504:}Jd*4noxl6-$0JXOQ8qcp-G?,mu3uD.P-^E%B0Ybf2g-h5Fg]#Ap.E-T3ekyFJdUT-{CbRzT58tu-BOMFh]xdD1-Y90)FPv0NZ-6hYERp{fqC-L,uwWP7vg5-Q2X6u)3o$S-2Jxv4sS7ek-#Y';
-        const webAppData = 'WebAppData';
-
-        p_token.textContent = 'Token: ' + token
-
-        async function hmacSha256(key, message) {
-            const encoder = new TextEncoder();
-            const keyData = encoder.encode(key);
-            const messageData = encoder.encode(message);
-
-            const cryptoKey = await crypto.subtle.importKey(
-                'raw',
-                keyData,
-                { name: 'HMAC', hash: 'SHA-256' },
-                false,
-                ['sign']
-            );
-
-            const signature = await crypto.subtle.sign('HMAC', cryptoKey, messageData);
-
-            // تبدیل امضای باینری به هگزادسیمال
-            return Array.from(new Uint8Array(signature))
-                .map(byte => byte.toString(16).padStart(2, '0'))
-                .join('');
-        }
-
-        // تابع اصلی برای پردازش داده‌ها و بررسی هش
-        async function processData(data) {
-            const params = new URLSearchParams(data);
-            const request = {};
-
-            // تجزیه داده‌ها به جفت‌های کلید-مقدار
-            for (let [key, value] of params) {
-                request[key] = value;
-            }
-
-            // مرتب‌سازی جفت‌ها بر اساس کلیدها
-            const sortedKeys = Object.keys(request).sort();
-
-            // استخراج hash و حذف آن از جفت‌های کلید-مقدار
-            const hash = request['hash'];
-            delete request['hash'];
-
-            // تبدیل رشته user به JSON
-            if (request['user']) {
-                request['user'] = JSON.parse(decodeURIComponent(request['user']));
-            }
-
-            // محاسبه HMAC-SHA256 برای WebAppData و توکن ربات
-            const hmacKey = await hmacSha256(webAppData, token);
-
-            // ساخت رشته نهایی از جفت‌های مرتب شده
-            const dataString = sortedKeys
-                .filter(key => key !== 'hash')
-                .map(key => `${key}=${typeof request[key] === 'object' ? JSON.stringify(request[key]) : request[key]}`)
-                .join('\n');
-
-            // محاسبه HMAC-SHA256 نهایی
-            const calculatedHash = await hmacSha256(hmacKey, dataString);
-
-            console.log('Hash:', hash);
-            console.log('Calculated Hash:', calculatedHash);
-
-            // بررسی تطابق هش‌ها
-            const isValid = hash === calculatedHash;
-            console.log('Is valid:', isValid);
-        }
-
-        processData(data);
 
 
     </script>
--- routes/web.php
+++ routes/web.php
@@ -10,41 +10,5 @@ Route::view('/eitaa', 'eitaa');
 Route::get('/re', function () {
     echo '<script src="https://developer.eitaa.com/eitaa-web-app.js"></script>';
 
-    $data = 'auth_date=1729767550&query_id=10111140801903956&user={"id":4332838,"first_name":"محمد مقداد","last_name":"فدایی","language_code":"fa"}&hash=76dc3c6e7693c39ec0209780e1da82e1ddc65157a0321804af83620356a2236b';
-    $data = 'user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3788475317572404878&chat_type=private&auth_date=1709144340&hash=371697738012ebd26a111ace4aff23ee265596cd64026c8c3677956a85ca1827';
-
-    parse_str($data, $request);
-//    $request['user'] = json_decode($request['user'], true);
-
-    ksort($request);
-
-    $request = collect($request);
-
-
-    $hash = $request->pull('hash');
-
-    $WebAppData = 'WebAppData';
-    $token = '5768337691:AAGDAe6rjxu1cUgxK4BizYi--Utc3J9v5AU';
-
-    $hmacKey = hash_hmac('sha256', $WebAppData, $token);
-
-    $dataString = $request
-        ->map(fn($value, $key) => "$key=$value")
-        ->implode("\n");
-
-    $calculatedHash = hash_hmac('sha256', $dataString, $hmacKey);
-
-    dd(
-        [
-            'Hash:' => $hash,
-            'Calculated Hash:' => $calculatedHash,
-            'Comparison Result:' => hash_equals($hash, $calculatedHash)
-        ]
-    );
-    dd(
-        hash_equals($hash, $calculatedHash),
-
-    );
-
     return view('eitaa');
 });
