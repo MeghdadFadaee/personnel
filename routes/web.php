7ef099bc9e80669faccb394f2cdf0cc60898f8be
Meghdad <mmf9484943@gmail.com>
2024-11-03 14:29:30 +0330

    remove alefbamehrabani landing page

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
-use Illuminate\Support\Arr;
 
 Route::view('/', 'welcome');
-Route::view('/eitaa', 'eitaa');
-
-Route::get('/re', function () {
-    echo '<script src="https://developer.eitaa.com/eitaa-web-app.js"></script>';
-
-    return view('eitaa');
-});
